2c42268acf51317088a7e3287f0bb4f956e90131
Diego Sánchez Escribano <sanchez.escribano.diego@gmail.com>
2023-12-08 11:49:55 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-python3 -m venv .venv
-source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
@@ -7,4 +5,3 @@ reflex export --frontend-only
 rm -fr public
 unzip frontend.zip -d public
 rm -f frontend.zip
-deactivate
